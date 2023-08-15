ae5986d6edd54b673adf7ed0e313f03cc830269b
subhro1530 <shaswata.ssaha@gmail.com>
2023-08-15 22:46:13 +0530

    changes saved

--- chatgpt-clone/script.js
+++ chatgpt-clone/script.js
@@ -1,3 +1,11 @@
+const API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
+const API_KEY = "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"; // Replace with your actual API key
+
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${API_KEY}`,
+};
+
 const chatBox = document.getElementById("chat-box");
 const userInput = document.getElementById("user-input");
 const sendButton = document.getElementById("send-button");
@@ -5,17 +13,33 @@ const sendButton = document.getElementById("send-button");
 sendButton.addEventListener("click", sendMessage);
 
 function sendMessage() {
-  const userMessage = userInput.value;
-  if (userMessage.trim() === "") return;
+  const userMessage = userInput.value.trim();
+  if (userMessage === "") return;
 
   appendMessage("user", userMessage);
   userInput.value = "";
 
-  // Simulate receiving a response from the chatbot (for demo purposes)
-  setTimeout(() => {
-    const botMessage = "This is a sample response from the chatbot.";
-    appendMessage("bot", botMessage);
-  }, 500);
+  const data = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: userMessage },
+    ],
+  };
+
+  fetch(API_ENDPOINT, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((responseData) => {
+      const assistantReply = responseData.choices[0].message.content;
+      appendMessage("bot", assistantReply);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function appendMessage(role, message) {
